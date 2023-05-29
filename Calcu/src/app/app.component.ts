b488ac579131d9673a6cbba87877db0d155ddcfe
GreenLuimonade <sanjunior08@gmail.com>
2023-05-28 23:40:54 -0500

    buttons

--- Calcu/src/app/app.component.ts
+++ Calcu/src/app/app.component.ts
@@ -6,5 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calcu';
+  title = 'uno';
+  var mensaje:any
+  uno(): void
+  {
+        
+  }
+  dos(): void
+  {
+        
+  }
+  tres(): void
+  {
+        
+  }
+  cuatro(): void
+  {
+        
+  }
+  cinco(): void
+  {
+        
+  }
+  seis(): void
+  {
+        
+  }
+  siete(): void
+  {
+        
+  }
+  ocho(): void
+  {
+        
+  }
+  nueve(): void
+  {
+        
+  }
+  cero(): void
+  {
+        
+  }
+  suma(): void
+  {
+        
+  }
+  multi(): void
+  {
+        
+  }
+  divi(): void
+  {
+        
+  }
+  resta(): void
+  {
+        
+  }
+  tan(): void
+  {
+        
+  }
+  sin(): void
+  {
+        
+  }
+  cos(): void
+  {
+        
+  }
+  ln(): void
+  {
+        
+  }
+  potencia(): void
+  {
+        
+  }
+  borrar(): void
+  {
+        
+  }
 }
